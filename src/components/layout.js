ad95b0e8969e5456f6c0152e090037a6e455938a
nealthom <nealthom@gmail.com>
2020-04-05 16:17:01 -0500

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,22 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Global, css } from "@emotion/core"
+import styled from "@emotion/styled"
 
-import Header from "./header"
-import "./layout.css"
+const Wrapper = styled("div")`
+  border: 2px solid green;
+  padding: 10px;
+`
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+export default ({ children }) => (
+  <Wrapper>
+    <Global
+      styles={css`
+        div {
+          background: red;
+          color: white;
         }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+      `}
+    />
+    {children}
+  </Wrapper>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const ImageContainer = styled.div`
+  max-width: 300px;
+  margin-bottom: 1.45rem;
+`
+const underline = css`
+  text-decoration: underline;
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 css={underline}>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <ImageContainer>
       <Image />
-    </div>
+    </ImageContainer>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
