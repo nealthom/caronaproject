d017dd03b8cf358dd9c708b2ed51b801bd8ea0eb
nealthom <nealthom@gmail.com>
2020-04-10 14:16:12 -0500

    working on overview page

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
 
 const Wrapper = styled("div")`
-  border: 2px solid green;
+  border: 2px solid purple;
   padding: 10px;
 `
 
@@ -12,8 +12,11 @@ export default ({ children }) => (
     <Global
       styles={css`
         div {
-          background: red;
+          background: pink;
           color: white;
+        }
+        section {
+          height: 90vh;
         }
       `}
     />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
-import styled from "@emotion/styled"
-import { css } from "@emotion/core"
+
+import CardList from "../components/CardList"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const ImageContainer = styled.div`
-  max-width: 300px;
-  margin-bottom: 1.45rem;
-`
-const underline = css`
-  text-decoration: underline;
-`
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1 css={underline}>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <ImageContainer>
-      <Image />
-    </ImageContainer>
-    <Link to="/page-2/">Go to page 2</Link>
+    <section>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="#description">view description</Link>
+    </section>
+    <section id="description">
+      <Link to="/">back to overview</Link>
+      <CardList />
+    </section>
   </Layout>
 )
 
